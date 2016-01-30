2f66974bd50318cb313a7ff814a03b22feab829f
kamil <kamil4892@interia.pl>
2016-01-30 14:26:14 +0100

    first django unit test passed, url mapping and empty view function for home_page

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,6 +1,11 @@
-from django.test import TestCase
+#!/usr/bin/env python3.4
 
-class SmokeTest(TestCase):
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
+from django.core.urlresolvers import resolve
+from django.test import TestCase
+from lists.views import home_page
+
+class HomePageTest(TestCase):
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
 
